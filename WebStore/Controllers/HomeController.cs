0c4402ec327bafb921013c5789e78b6a262fbc76
Infarh <shmachilin@yandex.ru>
2021-12-10 20:29:27 +0300

    Добавление первого и главного контролера

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Данные из первого контроллера");
+        }
+    }
+}
--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -2,12 +2,28 @@ var builder = WebApplication.CreateBuilder(args);
 
 //builder.Configuration.AddCommandLine(args);
 
+var services = builder.Services;
+services.AddControllersWithViews();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
 
 // Загрузка информации из файла конфигурации
 //var configuration = app.Configuration;
 //var greetings = configuration["CustomGreetings"];
 
-app.MapGet("/", () => app.Configuration["CustomGreetings"]);
+//app.MapGet("/", () => app.Configuration["CustomGreetings"]);
+app.MapGet("/throw", () =>
+{
+    throw new ApplicationException("Ошибка в программе!");
+});
+
+app.MapDefaultControllerRoute();
 
 app.Run();
