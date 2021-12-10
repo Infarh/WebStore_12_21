81757e75224d013fd1085b99aceb10e5e7d54cdc
Infarh <shmachilin@yandex.ru>
2021-12-10 20:12:49 +0300

    Добавление возможности чтения содержимого файла конфигурации

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Загрузка информации из файла конфигурации
+
+var configuration = app.Configuration;
+
+var greetings = configuration["CustomGreetings"];
+
+app.MapGet("/", () => greetings);
 
 app.Run();
