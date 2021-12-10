caed68d5ebae670a3761a25a33bd14569afe721e
Infarh <shmachilin@yandex.ru>
2021-12-10 20:20:47 +0300

    Рефакторинг

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,8 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//builder.Configuration.AddCommandLine(args);
+
 var app = builder.Build();
 
 // Загрузка информации из файла конфигурации
-
 //var configuration = app.Configuration;
 //var greetings = configuration["CustomGreetings"];
 
