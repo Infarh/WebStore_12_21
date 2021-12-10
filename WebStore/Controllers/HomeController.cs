c24662991c625aaeb0d18dbdd0ffbb9b0db44847
Infarh <shmachilin@yandex.ru>
2021-12-10 20:58:37 +0300

    Страничка со списком сотрудников

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,18 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
-namespace WebStore.Controllers
+namespace WebStore.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private static readonly List<Employee> __Employees = new()
     {
-        public IActionResult Index()
-        {
-            //return Content("Данные из первого контроллера");
-            return View();
-        }
+        new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 23 },
+        new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 27 },
+        new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
+    };
 
-        public string ConfiguredAction(string id, string Value1)
-        {
-            return $"Hello World! {id} - {Value1}";
-        }
+    public IActionResult Index()
+    {
+        //return Content("Данные из первого контроллера");
+        return View();
+    }
+
+    public string ConfiguredAction(string id, string Value1)
+    {
+        return $"Hello World! {id} - {Value1}";
+    }
+
+    public IActionResult Employees()
+    {
+        return View(__Employees);
     }
 }
