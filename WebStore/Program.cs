ab833b237a1c94107a918194f3f83e9611239fb0
Infarh <shmachilin@yandex.ru>
2021-12-10 20:16:51 +0300

    Добавлена возможность динамического изменения приветствия через файл конфигурации

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -3,10 +3,9 @@ var app = builder.Build();
 
 // Загрузка информации из файла конфигурации
 
-var configuration = app.Configuration;
+//var configuration = app.Configuration;
+//var greetings = configuration["CustomGreetings"];
 
-var greetings = configuration["CustomGreetings"];
-
-app.MapGet("/", () => greetings);
+app.MapGet("/", () => app.Configuration["CustomGreetings"]);
 
 app.Run();
