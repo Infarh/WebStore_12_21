470ce33dac926b4e6c6013fff54e44fcbf1862f8
Infarh <shmachilin@yandex.ru>
2021-12-14 20:31:04 +0300

    Контроллер Сотрудников

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private static readonly List<Employee> __Employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 23 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 27 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
+        };
+
+        public IActionResult Index()
+        {
+            var result = __Employees;
+            return View(result);
+        }
+    }
+}
--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,18 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using WebStore.Models;
 
 namespace WebStore.Controllers;
 
 public class HomeController : Controller
 {
-    private static readonly List<Employee> __Employees = new()
-    {
-        new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 23 },
-        new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 27 },
-        new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
-    };
-
-    public IActionResult Index()
+   public IActionResult Index()
     {
         //return Content("Данные из первого контроллера");
         return View();
@@ -21,10 +13,5 @@ public class HomeController : Controller
     public string ConfiguredAction(string id, string Value1)
     {
         return $"Hello World! {id} - {Value1}";
-    }
-
-    public IActionResult Employees()
-    {
-        return View(__Employees);
     }
 }
