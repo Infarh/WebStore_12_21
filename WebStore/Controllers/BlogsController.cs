6adb5f450e45244db1e9150ccf65f504b647328f
Infarh <shmachilin@yandex.ru>
2021-12-14 21:11:53 +0300

    Добавлены блоги

---
+++ WebStore/Controllers/BlogsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    //[Controller]
+    public class BlogsController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult Blog() => View();
+    }
+}
