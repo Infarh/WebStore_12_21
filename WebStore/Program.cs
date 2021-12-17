2ccb20b4fbc5ee4527005711ce5061abb0cd35ce
Infarh <shmachilin@yandex.ru>
2021-12-17 20:04:20 +0300

    Удаление хлама

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -4,10 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 #region Настройка построителя приложения - определение содержимого
 
-//builder.Configuration.AddCommandLine(args);
-
-//builder.Logging.AddFilter("Microsoft", LogLevel.Warning);
-
 var services = builder.Services;
 services.AddControllersWithViews(opt =>
 {
@@ -27,28 +23,14 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
-app.UseStaticFiles(/*new StaticFileOptions { ServeUnknownFileTypes = true }*/);
+app.UseStaticFiles();
 
 app.UseRouting();
 
-// Загрузка информации из файла конфигурации
-//var configuration = app.Configuration;
-//var greetings = configuration["CustomGreetings"];
-
-//app.MapGet("/", () => app.Configuration["CustomGreetings"]);
-app.MapGet("/throw", () =>
-{
-    throw new ApplicationException("Ошибка в программе!");
-});
-
-//app.MapDefaultControllerRoute();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}"); 
 
 #endregion
 
-// Запуск приложения
-
-//app.Start(); - не работает! Нужно Run()
 app.Run();
