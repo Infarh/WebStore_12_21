0a45ed199a6be7cbf762a0ea95a872d49f595a69
Infarh <shmachilin@yandex.ru>
2021-12-21 21:01:16 +0300

    Визуальная часть компонента секций

---
+++ WebStore/Components/BrandsViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Components;
+
+public class BrandsViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
