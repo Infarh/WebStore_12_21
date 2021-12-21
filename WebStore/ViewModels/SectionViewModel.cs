678c0757fa7eff793ffecf3a793e6c16e3d6d291
Infarh <shmachilin@yandex.ru>
2021-12-21 20:50:32 +0300

    Модель-представления секции

---
+++ WebStore/ViewModels/SectionViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WebStore.ViewModels;
+
+public class SectionViewModel
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public int Order { get; set; }
+
+    public SectionViewModel? Parent { get; set; }
+
+    public List<SectionViewModel> ChildSections { get; set; } = new();
+}
