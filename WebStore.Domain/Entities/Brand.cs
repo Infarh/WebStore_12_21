312784ee8086879319fc95a1a31049f4e37c54f8
Infarh <shmachilin@yandex.ru>
2021-12-21 20:33:27 +0300

    Сущности Секции и Бренда

---
+++ WebStore.Domain/Entities/Brand.cs
@@ -0,0 +1,9 @@
+﻿using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities;
+
+public class Brand : NamedEntity, IOrderedEntity
+{
+    public int Order { get; set; }
+}
