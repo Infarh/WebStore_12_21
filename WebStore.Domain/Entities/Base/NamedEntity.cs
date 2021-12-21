ee448ccb72ed0ebde8a4c53f5e557d342a6f5eeb
Infarh <shmachilin@yandex.ru>
2021-12-21 20:31:31 +0300

    Базовые абстрактные классы сущностей

---
+++ WebStore.Domain/Entities/Base/NamedEntity.cs
@@ -0,0 +1,8 @@
+﻿using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities.Base;
+
+public abstract class NamedEntity : Entity, INamedEntity
+{
+    public string Name { get; set; }
+}
