0ade19e93ebdc0d376f267f71c5e91d76e2f7f70
Infarh <shmachilin@yandex.ru>
2021-12-21 20:40:03 +0300

    Реализация сервиса каталога товаров + регистрация его в контейнере

---
+++ WebStore/Services/InMemoryProductData.cs
@@ -0,0 +1,12 @@
+﻿using WebStore.Data;
+using WebStore.Domain.Entities;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Services;
+
+public class InMemoryProductData : IProductData
+{
+    public IEnumerable<Section> GetSections() => TestData.Sections;
+
+    public IEnumerable<Brand> GetBrands() => TestData.Brands;
+}
