a44b82d0b0d07c1e744a99764ec71c79f7bab272
Infarh <shmachilin@yandex.ru>
2021-12-21 21:11:07 +0300

    Товар

---
+++ WebStore.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities;
+
+public class Product : NamedEntity, IOrderedEntity
+{
+    public int Order { get; set; }
+
+    public int SectionId { get; set; }
+
+    public int? BrandId { get; set; }
+
+    public string ImageUrl { get; set; }
+
+    public decimal Price { get; set; }
+}
