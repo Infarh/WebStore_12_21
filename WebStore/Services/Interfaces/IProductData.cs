2eb1b8dfb8d2b8f59707b06137998e10d171844f
Infarh <shmachilin@yandex.ru>
2021-12-21 20:35:19 +0300

    Интерфейс сервиса каталога товаров

---
+++ WebStore/Services/Interfaces/IProductData.cs
@@ -0,0 +1,10 @@
+﻿using WebStore.Domain.Entities;
+
+namespace WebStore.Services.Interfaces;
+
+public interface IProductData
+{
+    IEnumerable<Section> GetSections();
+
+    IEnumerable<Brand> GetBrands();
+}
