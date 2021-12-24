2452e677d1c24c62b3b3b7af5013d6799724a7da
Infarh <shmachilin@yandex.ru>
2021-12-24 20:25:12 +0300

    Определение контекста БД

---
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebStore.Domain.Entities;
+
+namespace WebStore.DAL.Context;
+
+public class WebStoreDB : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    public DbSet<Section> Sections { get; set; }
+
+    public DbSet<Brand> Brands { get; set; }
+
+    public WebStoreDB(DbContextOptions<WebStoreDB> options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder db)
+    {
+        base.OnModelCreating(db);
+
+        //db.Entity<Section>()
+        //   .HasMany(section => section.Products)
+        //   .WithOne(product => product.Section)
+        //   .OnDelete(DeleteBehavior.Cascade);
+    }
+}
