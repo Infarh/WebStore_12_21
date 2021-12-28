d3fa1b6e854189cf26d78d0ccda5774c61409fcd
Infarh <shmachilin@yandex.ru>
2021-12-28 19:54:08 +0300

    Заготовка контроллера Account

---
+++ WebStore/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers;
+
+public class AccountController : Controller
+{
+    public IActionResult Register() => View();
+
+    public IActionResult Login() => View();
+
+    public IActionResult Logout() => RedirectToAction("Index", "Home");
+
+    public IActionResult AccessDenied() => View();
+}
