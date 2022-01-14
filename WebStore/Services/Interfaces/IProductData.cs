778ba8deb87f1cafc4c921fffa2d2464d1e55530
Infarh <shmachilin@yandex.ru>
2022-01-14 20:21:08 +0300

    Доработка интерфейса сервиса каталога товаров - создание нового товара

--- WebStore/Services/Interfaces/IProductData.cs
+++ WebStore/Services/Interfaces/IProductData.cs
@@ -7,9 +7,15 @@ public interface IProductData
 {
     IEnumerable<Section> GetSections();
 
+    Section? GetSectionById(int Id);
+
     IEnumerable<Brand> GetBrands();
+
+    Brand? GetBrandById(int Id);
 
     IEnumerable<Product> GetProducts(ProductFilter? Filter = null);
 
     Product? GetProductById(int Id);
+
+    Product CreateProduct(string Name, int Order, decimal Price, string ImageUrl, string Section, string? Brand = null);
 }
