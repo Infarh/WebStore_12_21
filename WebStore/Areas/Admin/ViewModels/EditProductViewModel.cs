3f6cbd008fffda60c27dabe7523a85fb4312a4e6
Infarh <shmachilin@yandex.ru>
2022-01-14 20:09:39 +0300

    Логика контроллера управления каталогом товаров

---
+++ WebStore/Areas/Admin/ViewModels/EditProductViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace WebStore.Areas.Admin.ViewModels;
+
+public class EditProductViewModel
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public int Order { get; set; }
+
+    public int SectionId { get; set; }
+
+    public string Section { get; set; }
+
+    public int? BrandId { get; set; }
+
+    public string? Brand { get; set; }
+
+    public string ImageUrl { get; set; }
+
+    public decimal Price { get; set; }
+}
