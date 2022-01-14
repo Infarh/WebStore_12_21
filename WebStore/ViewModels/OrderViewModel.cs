613af87a554e4f15d5490d6c6d0b77491ec013ec
Infarh <shmachilin@yandex.ru>
2022-01-14 20:55:14 +0300

    Реализация логики сервиса заказов

--- WebStore/ViewModels/OrderViewModel.cs
+++ WebStore/ViewModels/OrderViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace WebStore.ViewModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebStore.ViewModels;
 
 public class OrderViewModel
 {
+    [Required]
+    public string Address { get; set; } = null!;
 
+    [Required]
+    [DataType(DataType.PhoneNumber)]
+    public string Phone { get; set; } = null!;
+
+    [MaxLength(200)]
+    public string? Description { get; set; }
 }
