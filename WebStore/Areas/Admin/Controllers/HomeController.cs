1c4450f844358484424e283af447e18b5aeff116
Infarh <shmachilin@yandex.ru>
2022-01-14 19:42:04 +0300

    Перекрёсные ссылки на область администратора

--- WebStore/Areas/Admin/Controllers/HomeController.cs
+++ WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities.Identity;
 
 namespace WebStore.Areas.Admin.Controllers;
 
-[Area("Admin")]
+[Area("Admin"), Authorize(Roles = Role.Administrators)]
 public class HomeController : Controller
 {
     public IActionResult Index() => View();
