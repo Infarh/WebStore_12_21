b72533b30854ad05573ff51362ab879d1724122e
Infarh <shmachilin@yandex.ru>
2022-01-14 19:55:46 +0300

    Редактор товаров

--- WebStore/Areas/Admin/Controllers/ProductsController.cs
+++ WebStore/Areas/Admin/Controllers/ProductsController.cs
@@ -1,8 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using WebStore.Domain.Entities.Identity;
+using WebStore.Services.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers;
 
+[Area("Admin"), Authorize(Roles = Role.Administrators)]
 public class ProductsController : Controller
 {
-    public IActionResult Index() => View();
+    private readonly IProductData _ProductData;
+    private readonly ILogger<ProductsController> _Logger;
+
+    public ProductsController(IProductData ProductData, ILogger<ProductsController> Logger)
+    {
+        _ProductData = ProductData;
+        _Logger = Logger;
+    }
+
+    public IActionResult Index()
+    {
+        var products = _ProductData.GetProducts();
+        return View(products);
+    }
 }
