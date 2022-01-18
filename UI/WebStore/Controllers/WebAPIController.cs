5643e784844f83c0514aa6f9e6f62e2353c3434e
Infarh <shmachilin@yandex.ru>
2022-01-18 21:19:58 +0300

    Визуальное управление клиентом тестового WebAPI

---
+++ UI/WebStore/Controllers/WebAPIController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.TestAPI;
+
+namespace WebStore.Controllers;
+
+public class WebAPIController : Controller
+{
+    private readonly IValuesService _ValuesService;
+
+    public WebAPIController(IValuesService ValuesService) => _ValuesService = ValuesService;
+
+    public IActionResult Index()
+    {
+        var values = _ValuesService.GetValues();
+        return View(values);
+    }
+}
