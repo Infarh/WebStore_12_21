99d2011e48567bd32c72d7bff8fb4fdc69880fba
Infarh <shmachilin@yandex.ru>
2022-01-18 20:32:19 +0300

    Проет-хостинг для нашего WebAPI

---
+++ Services/WebStore.WebAPI/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
