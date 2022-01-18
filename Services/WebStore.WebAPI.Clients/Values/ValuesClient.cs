24e0b2b8c3b882832dded233355c82ab0795934c
Infarh <shmachilin@yandex.ru>
2022-01-18 20:57:38 +0300

    Заготовка клиента тестового WebAPI

---
+++ Services/WebStore.WebAPI.Clients/Values/ValuesClient.cs
@@ -0,0 +1,24 @@
+﻿using WebStore.Interfaces.TestAPI;
+using WebStore.WebAPI.Clients.Base;
+
+namespace WebStore.WebAPI.Clients.Values;
+
+public class ValuesClient : BaseClient, IValuesService
+{
+    public ValuesClient(HttpClient Client) : base(Client, "api/values")
+    {
+        
+    }
+
+    public IEnumerable<string> GetValues() { throw new NotImplementedException(); }
+
+    public int Count() { throw new NotImplementedException(); }
+
+    public string GetById(int Id) { throw new NotImplementedException(); }
+
+    public void Add(string Value) { throw new NotImplementedException(); }
+
+    public void Edit(int Id, string Value) { throw new NotImplementedException(); }
+
+    public bool Delete(int Id) { throw new NotImplementedException(); }
+}
