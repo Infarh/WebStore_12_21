96aba5a4f725e066629942d08bdfbde31058516e
Infarh <shmachilin@yandex.ru>
2022-01-21 20:03:27 +0300

    Доработка базового клиента WebAPI

--- Services/WebStore.WebAPI.Clients/Base/BaseClient.cs
+++ Services/WebStore.WebAPI.Clients/Base/BaseClient.cs
@@ -1,4 +1,6 @@
-﻿namespace WebStore.WebAPI.Clients.Base;
+﻿using System.Net.Http.Json;
+
+namespace WebStore.WebAPI.Clients.Base;
 
 public abstract class BaseClient
 {
@@ -10,5 +12,37 @@ public abstract class BaseClient
     {
         Http = Client;
         this.Address = Address;
+    }
+
+    protected T? Get<T>(string url) => GetAsync<T>(url).Result;
+    protected async Task<T?> GetAsync<T>(string url)
+    {
+        var response = await Http.GetAsync(url).ConfigureAwait(false);
+        return await response
+           .EnsureSuccessStatusCode()
+           .Content
+           .ReadFromJsonAsync<T>()
+           .ConfigureAwait(false);
+    }
+
+    protected HttpResponseMessage Post<T>(string url, T value) => PostAsync<T>(url, value).Result;
+    protected async Task<HttpResponseMessage> PostAsync<T>(string url, T value)
+    {
+        var response = await Http.PostAsJsonAsync(url, value).ConfigureAwait(false);
+        return response.EnsureSuccessStatusCode();
+    }
+
+    protected HttpResponseMessage Put<T>(string url, T value) => PutAsync<T>(url, value).Result;
+    protected async Task<HttpResponseMessage> PutAsync<T>(string url, T value)
+    {
+        var response = await Http.PutAsJsonAsync(url, value).ConfigureAwait(false);
+        return response.EnsureSuccessStatusCode();
+    }
+
+    protected HttpResponseMessage Delete(string url) => DeleteAsync(url).Result;
+    protected async Task<HttpResponseMessage> DeleteAsync(string url)
+    {
+        var response = await Http.DeleteAsync(url).ConfigureAwait(false);
+        return response;
     }
 }
