6c6716912da89fe9803b760584d2d71af8f3b5ac
Infarh <shmachilin@yandex.ru>
2022-01-21 20:38:00 +0300

    Контроллер товаров для webAPI

--- Services/WebStore.WebAPI/Controllers/ProductsApiController.cs
+++ Services/WebStore.WebAPI/Controllers/ProductsApiController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain;
+using WebStore.Interfaces.Services;
 
 namespace WebStore.WebAPI.Controllers;
 
@@ -6,5 +8,65 @@ namespace WebStore.WebAPI.Controllers;
 [Route("api/products")]
 public class ProductsApiController : Controller
 {
+    private readonly IProductData _ProductData;
 
+    public ProductsApiController(IProductData ProductData) => _ProductData = ProductData;
+
+    [HttpGet("sections")] // GET -> http://localhost:5001/api/products/sections
+    public IActionResult GetSections()
+    {
+        var sections = _ProductData.GetSections();
+        return Ok(sections);
+    }
+
+    [HttpGet("sections/{Id}")] // GET -> http://localhost:5001/api/products/sections/5
+    public IActionResult GetSectionById(int Id)
+    {
+        var section = _ProductData.GetSectionById(Id);
+        if (section is null)
+            return NotFound();
+
+        return Ok(section);
+    }
+
+    [HttpGet("brands")] // GET -> http://localhost:5001/api/products/brands
+    public IActionResult GetBrands()
+    {
+        var brands = _ProductData.GetBrands();
+        return Ok(brands);
+    }
+
+    [HttpGet("brands/{Id}")] // GET -> http://localhost:5001/api/products/brands/5
+    public IActionResult GetBrandById(int Id)
+    {
+        var brand = _ProductData.GetBrandById(Id);
+        if (brand is null)
+            return NotFound();
+
+        return Ok(brand);
+    }
+
+    [HttpPost]
+    public IActionResult GetProducts(ProductFilter? Filter = null)
+    {
+        var products = _ProductData.GetProducts(Filter);
+        return Ok(products);
+    }
+
+    [HttpGet("{Id}")]
+    public IActionResult GetProductGyId(int Id)
+    {
+        var product = _ProductData.GetSectionById(Id);
+        if (product is null)
+            return NotFound();
+
+        return Ok(product);
+    }
+
+    [HttpPost("new/{Name}")]
+    public IActionResult CreateProduct(string Name, int Order, decimal Price, string ImageUrl, string Section, string? Brand = null)
+    {
+        var product = _ProductData.CreateProduct(Name, Order, Price, ImageUrl, Section, Brand);
+        return CreatedAtAction(nameof(GetProductGyId), new { product.Id }, product);
+    }
 }
