3828947a15fc453348b8498208e28d3f3fb8b7e0
Infarh <shmachilin@yandex.ru>
2022-02-01 20:16:04 +0300

    Тестироване метода, генерирующего исключение

--- Tests/WebStore.Tests/Controllers/HomeControllerTests.cs
+++ Tests/WebStore.Tests/Controllers/HomeControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -56,5 +57,53 @@ public class HomeControllerTests
         Assert.Equal(expected_string, actual_string); 
 
         #endregion
+    }
+
+    [TestMethod, ExpectedException(typeof(ApplicationException))]
+    public void Throw_thrown_ApplicationException()
+    {
+        const string exception_message = "Message";
+
+        var controller = new HomeController();
+
+        controller.Throw(exception_message);
+    }
+
+    [TestMethod]
+    public void Throw_thrown_ApplicationException_with_Message()
+    {
+        const string exception_message = "Message";
+
+        var controller = new HomeController();
+
+        Exception? exception = null;
+        try
+        {
+            controller.Throw(exception_message);
+        }
+        catch (Exception e)
+        {
+            exception = e;
+        }
+
+        var application_exception = Assert.IsType<ApplicationException>(exception);
+
+        var actual_exception_message = application_exception.Message;
+
+        Assert.Equal(exception_message, actual_exception_message);
+    }
+
+    [TestMethod]
+    public void Throw_thrown_ApplicationException_with_Message2()
+    {
+        const string exception_message = "Message";
+
+        var controller = new HomeController();
+
+        var application_exception = Assert.Throws<ApplicationException>(() => controller.Throw(exception_message));
+
+        var actual_exception_message = application_exception.Message;
+
+        Assert.Equal(exception_message, actual_exception_message);
     }
 }
