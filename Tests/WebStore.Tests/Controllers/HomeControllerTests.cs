addfe7ab8c6d1467eebeb17ed670db9949bbf1dc
Infarh <shmachilin@yandex.ru>
2022-02-01 19:59:30 +0300

    О структуре модульных тестов

--- Tests/WebStore.Tests/Controllers/HomeControllerTests.cs
+++ Tests/WebStore.Tests/Controllers/HomeControllerTests.cs
@@ -12,14 +12,28 @@ public class HomeControllerTests
     [TestMethod]
     public void ConfiguredAction_Returns_string_value()
     {
+        // A-A-A = Arrange - Act - Assert
+
+        #region Arrange
+        
         const string id = "123";
         const string value_1 = "QWE";
-        const string expected_string = $"Hello World! {id} - {value_1}";
+        const string expected_string = $"Hello World! {id} - {value_1}"; 
 
         var controller = new HomeController();
 
+        #endregion
+
+        #region Act
+        
         var actual_string = controller.ConfiguredAction(id, value_1);
 
-        Assert.Equal(expected_string, actual_string);
+        #endregion
+
+        #region Assert
+        
+        Assert.Equal(expected_string, actual_string); 
+
+        #endregion
     }
 }
