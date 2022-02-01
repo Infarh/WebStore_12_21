90055f8a67955c1bf1dfc9a43c5ad8f236876e1d
Infarh <shmachilin@yandex.ru>
2022-02-01 19:56:07 +0300

    Первый модульный тест

---
+++ Tests/WebStore.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+
+using WebStore.Controllers;
+using Assert = Xunit.Assert;
+
+namespace WebStore.Tests.Controllers;
+
+[TestClass]
+public class HomeControllerTests
+{
+    [TestMethod]
+    public void ConfiguredAction_Returns_string_value()
+    {
+        const string id = "123";
+        const string value_1 = "QWE";
+        const string expected_string = $"Hello World! {id} - {value_1}";
+
+        var controller = new HomeController();
+
+        var actual_string = controller.ConfiguredAction(id, value_1);
+
+        Assert.Equal(expected_string, actual_string);
+    }
+}
