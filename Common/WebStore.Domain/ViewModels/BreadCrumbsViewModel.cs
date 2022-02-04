90f021ae9a442616b3775345e7e28ea31997913a
Infarh <shmachilin@yandex.ru>
2022-02-04 21:03:20 +0300

    Логика формирования хлебных крошек

--- Common/WebStore.Domain/ViewModels/BreadCrumbsViewModel.cs
+++ Common/WebStore.Domain/ViewModels/BreadCrumbsViewModel.cs
@@ -1,6 +1,12 @@
-﻿namespace WebStore.Domain.ViewModels;
+﻿using WebStore.Domain.Entities;
+
+namespace WebStore.Domain.ViewModels;
 
 public class BreadCrumbsViewModel
 {
+    public Section? Section { get; set; }
 
+    public Brand? Brand { get; set; }
+
+    public string? Product { get; set; }
 }
