0155896f556dff6ad02769134db8b5b19bb968ca
Infarh <shmachilin@yandex.ru>
2022-02-04 20:35:47 +0300

    Подсветка активной секции и активного бренда в боковой навигационной панели

---
+++ Common/WebStore.Domain/ViewModels/SelectableSectionsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Domain.ViewModels
+{
+    public class SelectableSectionsViewModel
+    {
+        public IEnumerable<SectionViewModel> Sections { get; set; }
+
+        public int? SectionId { get; set; }
+
+        public int? ParentSectionId { get; set; }
+    }
+}
--- UI/WebStore/Components/BrandsViewComponent.cs
+++ UI/WebStore/Components/BrandsViewComponent.cs
@@ -10,7 +10,11 @@ public class BrandsViewComponent : ViewComponent
 
     public BrandsViewComponent(IProductData ProductData) => _ProductData = ProductData;
 
-    public IViewComponentResult Invoke() => View(GetBrands());
+    public IViewComponentResult Invoke(string BrandId)
+    {
+        ViewBag.BrandId = (int.TryParse(BrandId, out var id) ? id : (int?)null)!;
+        return View(GetBrands());
+    }
 
     private IEnumerable<BrandViewModel> GetBrands() =>
         _ProductData.GetBrands()
