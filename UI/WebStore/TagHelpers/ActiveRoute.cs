2422c2b66addc956dc24aba113d12258dd811f9e
Infarh <shmachilin@yandex.ru>
2022-02-04 20:02:08 +0300

    Логика таг-хелпера подсветки активной ссылки

--- UI/WebStore/TagHelpers/ActiveRoute.cs
+++ UI/WebStore/TagHelpers/ActiveRoute.cs
@@ -7,7 +7,8 @@ namespace WebStore.TagHelpers;
 [HtmlTargetElement(Attributes = AttributeName)]
 public class ActiveRoute : TagHelper
 {
-    private const string AttributeName = "is-active-route";
+    private const string AttributeName = "ws-is-active-route";
+    private const string IgnoreAction = "ws-ignore-action";
 
     [HtmlAttributeName("asp-controller")]
     public string Controller { get; set; }
@@ -24,7 +25,45 @@ public class ActiveRoute : TagHelper
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
         output.Attributes.RemoveAll(AttributeName);
+        //var ignore_action = output.Attributes.ContainsName(IgnoreAction);
+        var ignore_action = output.Attributes.RemoveAll(IgnoreAction);
 
+        if (IsActive(ignore_action))
+            MakeActive(output);
+    }
 
+    private bool IsActive(bool IgnoreAction)
+    {
+        var route_values = ViewContext.RouteData.Values;
+
+        var route_controller = route_values["controller"]?.ToString();
+        var route_action = route_values["action"]?.ToString();
+
+        if (!IgnoreAction && Action is { Length: > 0 } action && !string.Equals(action, route_action))
+            return false;
+
+        if (Controller is { Length: > 0 } controller && !string.Equals(controller, route_controller))
+            return false;
+
+        foreach (var (key, value) in RouteValues)
+            if (!route_values.ContainsKey(key) || route_values[key]?.ToString() != value)
+                return false;
+
+        return true;
+    }
+
+    private static void MakeActive(TagHelperOutput output)
+    {
+        var class_attribute = output.Attributes.FirstOrDefault(attr => attr.Name == "class");
+
+        if (class_attribute is null)
+            output.Attributes.Add("class", "active");
+        else
+        {
+            if (class_attribute.Value?.ToString()?.Contains("active") ?? false)
+                return;
+
+            output.Attributes.SetAttribute("class", $"{class_attribute.Value} active");
+        }
     }
 }
