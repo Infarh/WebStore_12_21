e5496736db66388609e4eb3a75ffe6f5a02bbcfa
Infarh <shmachilin@yandex.ru>
2022-02-08 19:17:16 +0300

    API-методы управления корзиной

--- UI/WebStore/Controllers/CartController.cs
+++ UI/WebStore/Controllers/CartController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+
 using WebStore.Domain.ViewModels;
 using WebStore.Interfaces.Services;
 
@@ -57,4 +58,26 @@ public class CartController : Controller
         ViewBag.OrderId = Id;
         return View();
     }
+
+    #region WebAPI
+
+    public IActionResult AddAPI(int Id)
+    {
+        _CartService.Add(Id);
+        return Json(new { Id, message = $"Товар {Id} был добавлен" });
+    }
+
+    public IActionResult DecrementAPI(int Id)
+    {
+        _CartService.Decrement(Id);
+        return Ok();
+    }
+
+    public IActionResult RemoveAPI(int Id)
+    {
+        _CartService.Remove(Id);
+        return Ok(new { Id, message = $"Товар {Id} был удалён" });
+    }
+
+    #endregion
 }
