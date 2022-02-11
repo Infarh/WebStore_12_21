7c96ffadd16cdc6ed45b6367fbfe5e82d171ae70
Infarh <shmachilin@yandex.ru>
2022-02-11 19:35:08 +0300

    Исправление ошибки разбиения на страницы каталога товаров

--- UI/WebStore/TagHelpers/Paging.cs
+++ UI/WebStore/TagHelpers/Paging.cs
@@ -30,7 +30,8 @@ public class Paging : TagHelper
         ul.AddCssClass("pagination");
 
         var url_helper = _UrlHelperFactory.GetUrlHelper(ViewContext);
-        for (var i = 1; i < PageModel.TotalPages; i++)
+        var total_pages = PageModel.TotalPages;
+        for (var i = 1; i <= total_pages; i++)
             ul.InnerHtml.AppendHtml(CreateElement(i, url_helper));
 
         output.Content.AppendHtml(ul);
